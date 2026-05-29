89db67ed1d8da2e89fb150da0078054136855db9
HomemadeRakija <karlo.boy2009@gmail.com>
2026-05-29 09:22:25 +0200

    laget hvit ball på spilleren, nå lage de der spilleren skal fly

--- Nonacontakainonagon.py
+++ Nonacontakainonagon.py
@@ -22,6 +22,7 @@ y = HEIGHT - size - 50
 
 player_pos = pygame.Vector2(x, 0)
 player_vel = pygame.Vector2(0, 0)
+predicted_vel = pygame.Vector2(0,0)
 gravity = 0.6
 on_ground = False
 dragging = False
@@ -41,7 +42,7 @@ while True:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             dragging = True
-            player_vel == 0
+            player_vel = pygame.Vector2(0,0)
         elif event.type == pygame.MOUSEBUTTONUP:
            if dragging:
             dragging = False
@@ -51,12 +52,15 @@ while True:
             if direction.length() > max_lenght:
                direction.scale_to_length (max_lenght)
             player_vel = direction * launch_power
+            predicted_vel = direction * launch_power
 
-            
+
     
     # Apply gravity
-    player_vel.y += gravity
-    player_pos+= player_vel
+    if not dragging:
+        player_vel.y += gravity
+        player_pos+= player_vel
+    
 
     # Collision with ground
     if player_pos[1]+ size >= ground_y:
@@ -64,7 +68,6 @@ while True:
             on_ground = True
             #friskjon
             player_vel = player_vel / friction
-    
 
 
 
@@ -76,6 +79,13 @@ while True:
 
     # Square
     pygame.draw.rect(screen, BLUE, (player_pos.x,player_pos.y, size, size))
+
+    #kalkulert turte av spilleren
+    for i in range(60):
+        predicted_vel.y += gravity
+        predicted_pos = player_pos.copy()
+
+    pygame.draw.circle(screen, WHITE, (int(predicted_pos.x + size / 2), int(predicted_pos.y + size / 2)), 4)
 
     pygame.display.flip()
     clock.tick(60)
